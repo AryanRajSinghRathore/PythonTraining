3fa2f2d6ae0e54b9360a0f52a314978d5ef29b30
Aryan Raj Singh Rathore <149179003+AryanRajSinghRathore@users.noreply.github.com>
2023-11-24 21:08:01 +0530

    Add files via upload

---
+++ OOPs/OOPs.py
@@ -0,0 +1,21 @@
+class student:
+    def __init__(self):
+        self.name = 'ARYAN RATHORE'
+        self.rollno = 19
+        self.marks = 90
+    def talk(self):
+        print('Hello i am', self.name)
+        print('My roll no. is '.self.rollno)
+        print('My marks is ',self.marks)
+
+
+
+s1 = student()
+print(s1.name)
+print(s1.rollno)
+print(s1.marks)
+        
+s1 = student()
+print(s1.name)
+print(s1.rollno)
+print(s1.marks)
